4eebb05d4891cf30bb629132f52ad579584d2d30
ahugo <hugo.andriamampianina@ynov.com>
2022-11-04 00:15:36 +0100

    .\test.py

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from scapy.all import *
 from scapy.all import IP, Ether, ARP, ICMP
 
 ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="10.5.1.0/24"), timeout=2)
-responses = ans.summary(lambda s,r: r.sprintf("%Ether.src% %ARP.psrc%") )
-print(responses)
+IP_MAC_responses = ans.summary(lambda s,r: r.sprintf("%Ether.src% %ARP.psrc%") )
+print(IP_MAC_responses)
