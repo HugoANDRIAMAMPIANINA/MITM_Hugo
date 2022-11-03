0587986619e90546b22e7071987433186baca0c7
ahugo <hugo.andriamampianina@ynov.com>
2022-11-04 00:52:26 +0100

    .\test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from scapy.all import *
-from scapy.all import IP, Ether, ARP, ICMP, hwsrc
+from scapy.all import IP, Ether, ARP, ICMP
 
 
 IP_MAC_responses = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="10.5.1.0/24"), timeout=2)
