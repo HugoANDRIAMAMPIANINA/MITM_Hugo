00c3b10a2f86392a9989cad7d62cc17c9465fc30
ahugo <hugo.andriamampianina@ynov.com>
2022-11-04 00:13:29 +0100

    .\test.py

--- test.py
+++ test.py
@@ -7,7 +7,3 @@ for i in range(1,255):
 ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="10.5.1.0/24"), timeout=2)
 responses = ans.summary(lambda s,r: r.sprintf("%Ether.src% %ARP.psrc%") )
 print(responses)
-
-ans2, unans2 = sr(IP(dst="192.168.1.0/24")/ICMP(), timeout=3)
-responses = ans.summary(lambda s,r: r.sprintf("%IP.src%") )
-print(responses)
