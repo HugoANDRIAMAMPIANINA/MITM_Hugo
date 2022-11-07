05bae92322b5824456da93558fda7a505b04b5b8
ahugo <hugo.andriamampianina@ynov.com>
2022-11-07 14:56:01 +0100

    .\test.py

--- test.py
+++ test.py
@@ -8,6 +8,12 @@ arp_packet = frame/ARP(pdst="10.5.1.0/24")
 IP_MAC_responses, unans = srp(arp_packet, timeout=2)
 
 print(IP_MAC_responses[0][1].hwsrc)
+print(IP_MAC_responses[0][1].psrc)
+print(IP_MAC_responses[0][2].hwsrc)
+print(IP_MAC_responses[0][2].psrc)
+print(IP_MAC_responses[0][3].hwsrc)
+print(IP_MAC_responses[0][3].psrc)
+
 
 #victim_IP = djjdjddjd
 
