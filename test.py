f81091815cda185520995e9608236ff66a8d015a
ahugo <hugo.andriamampianina@ynov.com>
2022-11-07 15:14:53 +0100

    .\test.py

--- test.py
+++ test.py
@@ -5,17 +5,16 @@ frame = Ether(dst="ff:ff:ff:ff:ff:ff")
 
 arp_packet = frame/ARP(pdst="10.5.1.0/24")
 
-IP_MAC_responses, unans = srp(arp_packet, timeout=2)[0]
+IP_MAC_responses, unans = srp(arp_packet, timeout=2)
 
+MAC_on_network = []
+IP_on_network = []
 
-print(IP_MAC_responses[0][1].hwsrc)
-print(IP_MAC_responses[0][1].psrc)
-print(IP_MAC_responses[1][1].hwsrc)
-print(IP_MAC_responses[1][1].psrc)
-print(IP_MAC_responses[2][1].hwsrc)
-print(IP_MAC_responses[2][1].psrc)
-print(len(IP_MAC_responses))
+for i in range(len(IP_MAC_responses)):
+    MAC_on_network.append(IP_MAC_responses[i][1].hwsrc)
+    IP_on_network.append(IP_MAC_responses[i][1].psrc)
 
+print(MAC_on_network, IP_on_network)
 
 #victim_IP = djjdjddjd
 
